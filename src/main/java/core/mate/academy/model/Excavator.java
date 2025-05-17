07dc22db1e40e874b5f27582cca8d7b6a54bd2f1
Artem Bohachenko <bogart2012g00g1e@gmail.com>
2025-05-17 23:30:58 +0300

    added fields

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,7 +5,17 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private String typeOfOil;
+
     public Excavator() {
+    }
+
+    public String getTypeOfOil() {
+        return typeOfOil;
+    }
+
+    public void setTypeOfOil(String typeOfOil) {
+        this.typeOfOil = typeOfOil;
     }
 
     @Override
--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -5,7 +5,17 @@ package core.mate.academy.model;
  * Do not remove no-field constructor
  */
 public class Truck extends Machine {
+    private int maxSpeed;
+
     public Truck() {
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
     }
 
     @Override
